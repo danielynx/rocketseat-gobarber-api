5a289bafd40bb2f4475a75803d8c401bb618132f
Daniel Backes <danielbackes@protonmail.com>
2020-07-11 22:51:26 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# GoBarber API
+
+Project developed copying the instructor coding during the Rocketseat Bootcamp Course.
+
+This is the API to support a project that will manage barbershop scheduling.
+
+## What I learned or did in this project?
---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (request, response) =>
+  response.json({ message: 'Hello GoBarber' }),
+);
+
+app.listen(3333, () => {
+  console.log('🚀️ Server started on port 3333! 🤑️😎️');
+});
