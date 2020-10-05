1aaabc574c9fa1bcce0739182fe7a9a20ec64000
Daniel Backes <danielbackes@protonmail.com>
2020-10-05 19:57:41 -0400

    refactor: :fire: Restructure the application architecture to follow DDD (Domain Drive Design)

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -4,10 +4,10 @@ import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import 'express-async-errors';
 
-import routes from './routes';
-import './database';
-import uploadConfig from './config/upload';
-import AppError from './errors/AppError';
+import uploadConfig from '@config/upload';
+import AppError from '@shared/errors/AppError';
+import routes from '@shared/infra/http/routes';
+import '@shared/infra/typeorm';
 
 const app = express();
 
