baeb228a98c0c679442efe6d678ea25cb1edf395
Daniel Backes <danielbackes@protonmail.com>
2020-10-08 16:54:28 -0400

    refactor: Create the StorageProvider to isolate the upload files algorithm

--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -5,6 +5,7 @@ import { injectable, inject } from 'tsyringe';
 import AppError from '@shared/errors/AppError';
 import User from '@modules/users/infra/typeorm/entities/User';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 import uploadConfig from '@config/upload';
 
 interface IRequest {
@@ -16,7 +17,10 @@ interface IRequest {
 class UpdateUserAvatarService {
   constructor(
     @inject('UsersRepository')
-    private usersRepository: IUsersRepository
+    private usersRepository: IUsersRepository,
+
+    @inject('StorageProvider')
+    private storageProvider: IStorageProvider
   ) { }
 
   public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
@@ -27,21 +31,12 @@ class UpdateUserAvatarService {
     }
 
     if (user.avatar) {
-      try {
-        const userAvatarFilePath = path.join(
-          uploadConfig.directory,
-          user.avatar,
-        );
-
-        await fs.promises.unlink(userAvatarFilePath);
-      } catch (err) {
-        if (err.code !== 'ENOENT') {
-          throw new AppError("It wasn't possible change user avatar image.");
-        }
-      }
+      await this.storageProvider.deleteFile(user.avatar);
     }
 
-    user.avatar = avatarFilename;
+    const filename = await this.storageProvider.saveFile(avatarFilename);
+
+    user.avatar = filename;
 
     await this.usersRepository.update(user);
 
