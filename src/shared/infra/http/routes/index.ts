3f0588b0a20955ee5f1d83560ce5599102913aee
Daniel Backes <danielbackes@protonmail.com>
2020-10-13 22:25:20 -0400

    feat: Implementate the token generation and reset password

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,10 +3,12 @@ import { Router } from 'express';
 import appointmentsRouter from '@modules/appointments/infra/http/routes/appointments.routes';
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
+import passwordsRouter from '@modules/users/infra/http/routes/passwords.routes';
 
 const routes = Router();
 routes.use('/appointments', appointmentsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
+routes.use('/passwords', passwordsRouter);
 
 export default routes;
