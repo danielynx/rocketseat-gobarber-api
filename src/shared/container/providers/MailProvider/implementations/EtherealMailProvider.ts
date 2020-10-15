0b6bdb1a77d98fa397ac4ec99a3ca1b56a1c57b1
Daniel Backes <danielbackes@protonmail.com>
2020-10-15 17:06:12 -0400

    fix(mail): Change EtherealMailProvider to use the Promise strategy as same as Nodemailer library is built

--- src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -18,46 +18,49 @@ export default class EtherealMailProvider implements IMailProvider {
     subject,
     templateData,
   }: ISendMailDTO): Promise<void> {
-    await nodemailer.createTestAccount(async (err, account) => {
-      if (err) {
-        console.error(`Failed to create a testing account. ${err.message}`);
-        return process.exit(1);
-      }
-
-      console.log('Credentials obtained, sending message...');
-
-      // Create a SMTP transporter object
-      const transporter = await nodemailer.createTransport({
-        host: account.smtp.host,
-        port: account.smtp.port,
-        secure: account.smtp.secure,
-        auth: {
-          user: account.user,
-          pass: account.pass,
-        },
-      });
-
-      const message: SendMailOptions = {
-        from: {
-          name: from?.name || 'Equipe GoBarber',
-          address: from?.email || 'equipe@gobarber.com.br',
-        },
-        to: {
-          name: to.name,
-          address: to.email,
-        },
-        subject,
-        html: await this.mailTemplateProvider.parse(templateData),
-      };
-
-      await transporter.sendMail(message, (err, info) => {
+    return new Promise((resolve, reject) => {
+      nodemailer.createTestAccount(async (err, account) => {
         if (err) {
-          console.log(`Error occurred. ${err.message}`);
-          return process.exit(1);
+          console.error(`Failed to create a testing account. ${err.message}`);
+          reject();
         }
 
-        console.log('Message sent: %s', info.messageId);
-        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+        console.log('Credentials obtained, sending message...');
+
+        // Create a SMTP transporter object
+        const transporter = nodemailer.createTransport({
+          host: account.smtp.host,
+          port: account.smtp.port,
+          secure: account.smtp.secure,
+          auth: {
+            user: account.user,
+            pass: account.pass,
+          },
+        });
+
+        const message: SendMailOptions = {
+          from: {
+            name: from?.name || 'Equipe GoBarber',
+            address: from?.email || 'equipe@gobarber.com.br',
+          },
+          to: {
+            name: to.name,
+            address: to.email,
+          },
+          subject,
+          html: await this.mailTemplateProvider.parse(templateData),
+        };
+
+        transporter.sendMail(message, (err, info) => {
+          if (err) {
+            console.log(`Error occurred. ${err.message}`);
+            reject();
+          }
+
+          console.log('Message sent: %s', info.messageId);
+          console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+          resolve();
+        });
       });
     });
   }
