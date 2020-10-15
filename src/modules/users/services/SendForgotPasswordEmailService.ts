def62e57c9c9f805671898979a06abd7cb7c276e
Daniel Backes <danielbackes@protonmail.com>
2020-10-15 17:31:01 -0400

    feat(mail): Create a template to the forgot password e-mail

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,4 +1,5 @@
 import { injectable, inject } from 'tsyringe';
+import path from 'path';
 
 import AppError from '@shared/errors/AppError';
 
@@ -32,6 +33,13 @@ class SendForgotPasswordEmailService {
 
     const { token } = await this.userTokensRepository.generate(user.id);
 
+    const forgotPasswordTemplate = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'forgot_password.hbs',
+    );
+
     await this.mailProvider.sendMail({
       to: {
         name: user.name,
@@ -39,11 +47,10 @@ class SendForgotPasswordEmailService {
       },
       subject: '[GoBarber] Password rescue request',
       templateData: {
-        template:
-          'Hello, {{name}}. Here the token to reset your password: {{token}}',
+        file: forgotPasswordTemplate,
         variables: {
           name: user.name,
-          token,
+          link: `http://localhost:3000/reset_password?token=${token}`,
         },
       },
     });
