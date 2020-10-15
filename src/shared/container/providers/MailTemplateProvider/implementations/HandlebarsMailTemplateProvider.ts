e802a3cb9be5cf9978cf78221efe8e3f52155490
Daniel Backes <danielbackes@protonmail.com>
2020-10-15 17:25:32 -0400

    feat(mail): Change the MailTemplateProvider to request the path of the file that contain the mail template to be parsed

--- src/shared/container/providers/MailTemplateProvider/implementations/HandlebarsMailTemplateProvider.ts
+++ src/shared/container/providers/MailTemplateProvider/implementations/HandlebarsMailTemplateProvider.ts
@@ -1,18 +1,21 @@
 import handlebars from 'handlebars';
+import fs from 'fs';
 
 import IParseMailTemplateDTO from '@shared/container/providers/MailTemplateProvider/dtos/IParseMailTemplateDTO';
 import IMailTemplateProvider from '@shared/container/providers/MailTemplateProvider/models/IMailTemplateProvider';
 
 class HandlebarsMailTemplateProvider implements IMailTemplateProvider {
   public async parse({
-    template,
+    file,
     variables,
   }: IParseMailTemplateDTO): Promise<string> {
-    const parseTemplate = await handlebars.compile(template);
+    const templateFileContent = await fs.promises.readFile(file, {
+      encoding: 'utf-8',
+    });
 
-    const parsedTemplate = await parseTemplate(variables);
+    const parseTemplate = handlebars.compile(templateFileContent);
 
-    return parsedTemplate;
+    return parseTemplate(variables);
   }
 }
 
