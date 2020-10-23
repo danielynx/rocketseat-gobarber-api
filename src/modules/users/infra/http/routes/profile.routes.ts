ca64f7444ae02b068eda5516e8dc847284af4897
Daniel Backes <danielbackes@protonmail.com>
2020-10-22 20:14:18 -0400

    feat: Validate route's body parameters

--- src/modules/users/infra/http/routes/profile.routes.ts
+++ src/modules/users/infra/http/routes/profile.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import ProfileController from '@modules/users/infra/http/controllers/ProfileController';
 
@@ -10,6 +11,26 @@ const profileController = new ProfileController();
 profileRouter.use(ensureAuthenticated);
 
 profileRouter.get('/', profileController.show);
-profileRouter.put('/', profileController.update);
+profileRouter.put(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string(),
+      email: Joi.string().email(),
+      password: Joi.string(),
+      old_password: Joi.when('password', {
+        is: Joi.exist(),
+        then: Joi.string().required(),
+        otherwise: Joi.forbidden(),
+      }),
+      password_confirmation: Joi.when('password', {
+        is: Joi.exist(),
+        then: Joi.string().required().valid(Joi.ref('password')),
+        otherwise: Joi.forbidden(),
+      }),
+    },
+  }),
+  profileController.update,
+);
 
 export default profileRouter;
--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import multer from 'multer';
 import uploadConfig from '@config/upload';
+import { celebrate, Segments, Joi } from 'celebrate';
 
-import UsersController from "@modules/users/infra/http/controllers/UsersController";
-import UserAvatarController from "@modules/users/infra/http/controllers/UserAvatarController";
+import UsersController from '@modules/users/infra/http/controllers/UsersController';
+import UserAvatarController from '@modules/users/infra/http/controllers/UserAvatarController';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
@@ -13,7 +14,17 @@ const userAvatarController = new UserAvatarController();
 
 const upload = multer(uploadConfig);
 
-usersRouter.post('/', usersController.create);
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  usersController.create,
+);
 
 usersRouter.patch(
   '/avatar',
